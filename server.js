3d1d727e52f361c00306435f3b1263c29e0b4734
Van De Castro <vandecastro95@gmail.com>
2019-07-27 15:17:20 -0500

    fixed header to changed country per select

--- server.js
+++ server.js
@@ -20,5 +20,5 @@ app.get('*', (req,res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 })
 
-const PORT = process.envPORT || 5000;
+const PORT = process.envPORT || 8888;
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
