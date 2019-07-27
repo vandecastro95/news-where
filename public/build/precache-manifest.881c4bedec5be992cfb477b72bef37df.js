c37856dd3c0ff0291c3940dd7510d493149f244e
Van De Castro <vandecastro95@gmail.com>
2019-07-27 16:58:08 -0500

    changed public directory for deployment

---
+++ public/build/precache-manifest.881c4bedec5be992cfb477b72bef37df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a61d1e9044a28396971be372c398494",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0bb6a09d126593d0cae",
+    "url": "/static/css/main.941d9489.chunk.css"
+  },
+  {
+    "revision": "48d306629f0b99d13b95",
+    "url": "/static/js/2.02815b73.chunk.js"
+  },
+  {
+    "revision": "c0bb6a09d126593d0cae",
+    "url": "/static/js/main.29eebb1d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ public/precache-manifest.5700fa945b56a37424368d00bdd77620.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39a93ff6d4ab0ed6fbfed55340f6213e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "174904bd879e4a9d8973",
+    "url": "/static/css/main.941d9489.chunk.css"
+  },
+  {
+    "revision": "48d306629f0b99d13b95",
+    "url": "/static/js/2.02815b73.chunk.js"
+  },
+  {
+    "revision": "174904bd879e4a9d8973",
+    "url": "/static/js/main.4b84c9f0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
