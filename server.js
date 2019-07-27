f3c237054f9990e990718207affe5b8e92c433cd
Van De Castro <vandecastro95@gmail.com>
2019-07-27 17:04:56 -0500

    changed port for deployment

--- server.js
+++ server.js
@@ -20,5 +20,5 @@ app.get('*', (req,res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 })
 
-const PORT = process.envPORT || 8888;
+const PORT = process.env.PORT || 8888;
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
